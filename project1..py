641d5df99de96b0c1311bd0126f868e6a5ba6551
ITKiDS-Pygame <michael@itkids.club>
2024-01-09 20:32:18 +0900

    Battle Game

--- project1..py
+++ project1..py
@@ -3,7 +3,10 @@ import time
 
 def dice():
     roll = random.randrange(1,7)
-    return roll
+    if roll < 6:
+        return roll
+    else:
+        return roll *5
 
 dice_rool = dice()
 print(dice_rool)
@@ -16,11 +19,6 @@ enemy_life = 0
 
 def init():
     global player_life, enemy_life
-    player_life = 100
-    enemy_life = 100
-
-def init():
-    global player_life, enemy_life
     player_life, enemy_life = 100, 100
 
 def run():
@@ -29,4 +27,32 @@ def run():
 init()
 
 while True:
-    print( )
+    print("敵が現れた!")
+    time.sleep(1)
+    print("バトルの準備をしましょう!")
+    time.sleep(1)
+    choice = input("[A]戦う [R]逃げる: ").lower()
+    time.sleep(1)
+    if choice == "r":
+        print("逃げてみる...")
+        if dice() > 4:
+            print("逃げた!GAME OVER! ")
+            exit(1)
+    else:
+        print("逃げられなかった!")
+        damage = dice()
+        player_life -= damage
+        print(f"ダメージ: {damage}")
+        print(f"HP: {player_life}")
+        time.sleep(1)
+        print("敵はあなたにアタック!")
+        if dice() > dice():
+            print("敵のアタックが成功")
+            damage = dice()
+
+                  
+
+        
+
+
+        
