eee825f6df9d5b980339e676d7d4cb4f2ad2afde
ITKiDS-Pygame <michael@itkids.club>
2024-03-26 20:28:20 +0900

    20240326

---
+++ 11.py
@@ -0,0 +1,63 @@
+import pygame
+
+SCREEN_X = 640
+SCREEN_Y = 480
+SCREEN = (SCREEN_X, SCREEN_Y)
+BG = (0,0,0)
+FPS = 60
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+YELLOW = (255,255,0)
+current_color = BLUE
+
+surface = pygame.display.set_mode(SCREEN)
+pygame.display.set_caption("しかく練習")
+CLOCK = pygame.time.Clock()
+
+
+center_x = SCREEN_X / 2
+center_y = SCREEN_Y / 2
+center = (center_x, center_y)
+sides = 50
+start_x = center_x - sides / 2
+start_y = center_y - sides / 2
+myRect = pygame.Rect(start_x, start_y, sides, sides)
+
+def draw():
+    pygame.draw.rect(surface, current_color, myRect)
+    
+
+def update():
+    global current_color
+    CLOCK.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit(1)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        exit(1)
+    if keys[pygame.K_r]:
+        current_color = RED
+    if keys[pygame.K_y]:
+        current_color = YELLOW
+    if keys[pygame.K_b]:
+        current_color = BLUE
+    if keys[pygame.K_w]:
+        current_color = WHITE
+    if keys[pygame.K_g]:
+        current_color = GREEN
+
+    draw()
+
+    pygame.display.update()
+    surface.fill(BG)
+
+def run():
+    while True:
+        update()
+
+if __name__=='__main__':
+    run()
